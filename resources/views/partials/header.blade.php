2c8e47e1ed650a88cbc38e2cc83e24a07206c6e9
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-03-09 16:09:14 +0100

    style for header and footer

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="header-container">
+
+      
+        <div class="logo">
+            <!--carico il logo con asset-->
+            <img src="{{ asset('img/dc-logo.png')}}" alt="logo">
+        </div>
+        
+
+       
+        <nav class="navbar">
+            <ul>
+                <li><a href="#"></a>characters</li>
+                <li><a href="#"></a>comics</li>
+                <li><a href="#"></a>movies</li>
+                <li><a href="#"></a>tv</li>
+                <li><a href="#"></a>games</li>
+                <li><a href="#"></a>collectibles</li>
+                <li><a href="#"></a>videos</li>
+                <li><a href="#"></a>fans</li>
+                <li><a href="#"></a>news</li>
+                <li><a href="#"></a>shop</li>
+            </ul>
+        </nav>
+       
+    </div>
+</header>
