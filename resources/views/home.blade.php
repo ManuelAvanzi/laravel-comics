7307f7ec18548c6d86b9b473fe27997b62a55820
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-03-09 16:10:41 +0100

    main in home page style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,25 @@
 @endsection
 
 @section('content')
- <h2>Laravel comics</h2>
 
- <ul>
+<main>
+    <div class="cards-container">
 
-    <!--stampo il titolo e l'immagine-->
-
-    @foreach($data as $fumetto)
-    <li>  {{$fumetto["title"]}}</li>
-    <img src= {{$fumetto["thumb"] }}>
-    @endforeach
-</ul>
+    
+        <!--stampo il titolo e l'immagine-->
+        @foreach($data as $fumetto)
+        <div class="card">
+            <img src= {{$fumetto["thumb"] }}>
+            <li>  {{$fumetto["title"]}}</li>
+        </div>
+            
+            
+        @endforeach
+       
 @endsection
+
+    </div>
+
+
+</main>
+ 
